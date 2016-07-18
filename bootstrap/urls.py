b6ee88dda05111c79dcfe405a9eb31e3b568b1a8
gryevns <garyevans1990@gmail.com>
2016-07-18 11:42:21 +0100

    tabs

--- bootstrap/urls.py
+++ bootstrap/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-	url(r'^api/', include('api.urls')),
+    url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', TemplateView.as_view(template_name='index.html')),
 ]
